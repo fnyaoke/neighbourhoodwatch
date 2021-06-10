834c968a01b0730a89fd6f0b2e60842663cf93f0
fnyaoke <fnyaoke@gmail.com>
2021-06-10 10:07:42 +0300

    registered tables

---
+++ watch/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(Business)
+admin.site.register(User)
+
+#Action Admin
+
+def make_published(neighborhoodadmin, request, queryset):
+    queryset.update(status='p')
+    make_published.short_description = "Mark verified Neighborhoods published"
+
+class NeighborhoodAdmin(admin.ModelAdmin):
+    list_display = ['name', 'status']
+    ordering = ['name']
+    actions = [make_published]
+
+
+
+admin.site.register(Neighborhood, NeighborhoodAdmin)

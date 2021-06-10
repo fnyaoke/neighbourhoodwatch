d8218d2486bb1cc1443a624d15b5c3a0d551e59c
fnyaoke <fnyaoke@gmail.com>
2021-06-10 10:08:51 +0300

    added urls

---
+++ watch/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns=[
+  path('api/neighbors/',views.NeighborhoodList.as_view(),name='neighbor'),
+  path('api/business/',views.BusinessList.as_view(),name='business'),
+  path('api/users/',views.UserList.as_view(),name='users'),
+  path('api/update/users/<int:pk>/',views.UserList.as_view(),name='update_users'),
+  path('api/delete/users/<int:pk>/',views.UserList.as_view(),name='delete_users'),
+  re_path('api/update/business/(?P<pk>[0-9]+)/',views.BusinessList.as_view(),name='update_business'),
+  path('api/delete/business/<int:pk>/',views.BusinessList.as_view(),name='delete_business'),
+  path('api/update/neighbors/<int:pk>/',views.NeighborhoodList.as_view(),name='update_neighbors'),
+  re_path('api/delete/(?P<pk>[0-9]+)/',views.NeighborhoodList.as_view(),name='delete_neighbors')
+]

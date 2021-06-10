8ee819d0975dfecf1f167753a03b7dae056596e8
fnyaoke <fnyaoke@gmail.com>
2021-06-10 10:15:59 +0300

    testing user model

--- watch/tests.py
+++ watch/tests.py
@@ -29,3 +29,31 @@ class NeighborhoodTestCase(TestCase):
         self.new_neighborhood.delete_neighborhood()
         neighborhood = Neighborhood.objects.all()
         self.assertTrue(len(neighborhood)==0)
+
+class UserTestCase(TestCase):
+    def test_user(self):
+        self.assertTrue
+
+    def test_save_user(self):
+        self.assertTrue
+    #Set up method
+
+    def setUp(self):
+        self.new_user = User(name="Your Name", email="youremail@domain.com", password="my password")
+        self.new_user.save()
+        self.new_neighborhood = User(user=self.new_user, email = "myemail@domain.com", neighborhood="your location")
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_user,User))
+
+    def test_save_method(self):
+        self.new_user.save_user()
+        user = User.objects.all()
+        self.assertTrue(len(user)>0)
+
+    def test_delete_method(self):
+        self.new_user.save_user()
+        self.new_user.delete_user()
+        user = User.objects.all()
+        self.assertTrue(len(user)==0)
